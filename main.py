844e8261d20922113e437648661d726a4ee4dbdc
Adam <akalinowski44@github.com>
2018-07-12 12:26:05 +0200

    code refactor, add board functionality added

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
 @app.route("/", methods=['GET','POST'])
 def game():
     return render_template('index.html')
 
-
 def main():
-    app.run(debug=True)
+        app.run(
+        host='0.0.0.0',
+        port=8000,
+        debug=True,
+    )
 
 if __name__ == '__main__':
     main()
--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,19 @@
 (function() {
 
-    const columnContainer = document.getElementById("column-container");
-    const addColumnButton = document.getElementById("add-column");
-    const columnmodal = document.getElementById("modal");
+
+    const modal = document.getElementById("modal");
     const form = document.getElementById("modal-form");
     const cardModal = document.getElementById("card-modal");
     const cardForm = document.getElementById("card-form");
-    
 
+    const project = document.getElementById("project");
+    const addBoardButton = document.getElementById("add-board");
+    const boardModal = document.getElementById("board-modal");
+    const boardForm = document.getElementById("board-form");
+    const columnModal = document.getElementById("column-modal");
+    const columnForm = document.getElementById("column-form");
+    
+    let boards = [];
     let columns = [];
     let cards = [];
     let ids = [0];
@@ -20,54 +26,120 @@
         return element;
     }
 
-    function addColumn(columnName) {
+
+    addBoardButton.addEventListener("click", ()=>{
+        showBoardModal();
+    });
+
+    function showBoardModal() {
+        boardModal.style.display = "block";
+        const inputName = document.getElementById("board-name");
+        inputName.focus();
+    }
+
+    boardForm.addEventListener("submit", ()=>{
+        event.preventDefault();
+        let boardName = event.target.elements["board-name"];
+        addBoard(boardName.value);
+        boardName.value = "";
+        boardModal.style.display = "none";
+    });
+
+
+    function addBoard(boardName) {
+        let boardNode = cloneElement("board-template");
+
+        let id = generateId();
+        
+        let boardHeader = boardNode.querySelector(".board-header");
+        boardHeader.innerText = boardName;
+
+        let addColumnButton = boardNode.querySelector(".add-column");
+
+        addColumnButton.addEventListener("click", ()=>{
+            showColumnModal(id);
+        });
+
+        
+        boardNode.setAttribute("id", id);
+
+        let button = boardNode.querySelector(".remove-board");
+        button.setAttribute("data-id", id);
+
+        let columnContainer = boardNode.querySelector(".column-container");
+
+        let board = {
+            id: id,
+            name: boardName,
+            el: boardNode,
+            columnContainer: columnContainer
+        }
+
+        boards.push(board);
+        project.appendChild(boardNode);
+    }
+
+    function showColumnModal(boardId){
+        columnModal.style.display = "block";
+        const inputName = document.getElementById("column-name");
+        columnForm.elements["board-id"].value = boardId;
+        inputName.focus();
+    }
+
+    columnForm.addEventListener("submit", ()=>{
+        event.preventDefault();
+        let boardId = event.target.elements["board-id"].value;
+        let columnName = event.target.elements["column-name"].value;
+        addColumn(columnName, boardId);
+        columnName.value = "";
+        columnModal.style.display = "none";
+    });
+
+
+
+    function addColumn(columnName, boardId) {
         
         let columnNode = cloneElement("column-template");
+
+        let id = generateId();
 
         let columnHeader = columnNode.querySelector(".column-header");
         columnHeader.innerText = columnName;
         
         let cardContainer = columnNode.querySelector(".card-container");
 
-        let addCardButton = columnNode.getElementsByClassName("add-card")[0];
+        let addCardButton = columnNode.querySelector(".add-card");
 
         addCardButton.addEventListener("click", ()=>{
 
-        showCardModal(columnName);
+            showCardModal(id);
 
         });
 
-        let id = generateId();
+        
         columnNode.setAttribute("id", id);
 
-        let button = columnNode.getElementsByClassName("remove-column")[0];
-        button.setAttribute("data-id", id)
+        let button = columnNode.querySelector(".remove-column");
+        button.setAttribute("data-id", id);
 
         let column = {
             id: id,
             name: columnName,
             el: columnNode,
-            cardContainer: cardContainer
+            cardContainer: cardContainer,
+            boardId: boardId
         };
 
+        let board = document.getElementById(boardId);
+        let columnContainer = board.querySelector(".column-container");
+        console.log(columnContainer, boardId); 
         columnContainer.appendChild(columnNode);
-        return column;
+        columns.push(column);
     }
 
-
-    form.addEventListener("submit", (event)=>{
-        event.preventDefault();
-        let columnName = event.target.elements["modal-text"];
-        columns.push(addColumn(columnName.value));
-        columnName.value = "";
-
-        modal.style.display = "none";
-        
-    });
-
-    function showCardModal(columnName){
+    function showCardModal(columnId){
         cardModal.style.display = "block";
-        cardForm.elements["column-name"].value = columnName;
+        cardForm.elements["column-id"].value = columnId;
 
         const inputName = document.getElementById("card-name");
         inputName.focus();
@@ -77,12 +149,12 @@
         event.preventDefault();
         let name = event.target.elements["card-name"].value;
         let description = event.target.elements["card-description"].value;
-        let columnName = event.target.elements["column-name"].value;
+        let columnName = event.target.elements["column-id"].value;
         addCard(name, description, columnName);
         cardModal.style.display = "none";
     });
 
-    function addCard(name, description, columnName) {
+    function addCard(name, description, columnId) {
         let cardNode = cloneElement("card-template");
         let cardName = cardNode.querySelector(".card-name");
         let cardDescription = cardNode.querySelector(".card-description");
@@ -101,27 +173,18 @@
             id: id,
             name: name,
             description: description,
-            columnName: null,
+            columnId: columnId,
             el: cardNode
         }
 
-        addCardToColumn(card, columnName);
+        let column = document.getElementById(columnId);
+        let cardContainer = column.querySelector(".card-container");
+        cardContainer.appendChild(cardNode);
         cards.push(card);   
     }
 
-    function addCardToColumn(card, columnName){
-        if (card.columnName) {
-            //remove from column
-        } else {
-            card.columnName = columnName;
 
-            for (let column of columns) {
-                if (column.name === columnName) {
-                    column.cardContainer.appendChild(card.el);
-                }
-            }
-        }
-    }
+
 
     function removeCard(id) {
         for (i = 0; i < cards.length; i++) {
@@ -175,17 +238,22 @@
         }
     });
 
-    addColumnButton.addEventListener("click", ()=>{
+ /*   addColumnButton.addEventListener("click", ()=>{
         modal.style.display = "block";
         const inputTitle = document.getElementById("modal-text");
         inputTitle.focus();
-    });
+    });*/
     
 
     window.addEventListener("click", (event)=>{
-        if (event.target == modal) {
-            modal.style.display = "none";
+        if (event.target == boardModal) {
+            boardModal.style.display = "none";
+        } else if (event.target == columnModal) {
+            columnModal.style.display = "none";
+        } else if (event.target == cardModal) {
+            cardModal.style.display = "none";
         }
+
     });
 
     function generateId() {
@@ -199,5 +267,32 @@
             return 1;
         }
     }
+
+    
+
+
+
+
+    localStorage.setItem('tekst', 'costamcostam');
+    localStorage.setItem('tab', [1,2,3,4]);
+    console.log(localStorage.getItem('tekst'), localStorage.getItem('tab'));
+    
     
 })();
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    let data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+    let columnId = ev.target.parentElement.id;
+    console.log(columnId);
+    
+}
